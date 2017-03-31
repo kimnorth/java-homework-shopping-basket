760591f37d37d85907e63d0320fa9486d30e98cd
kimnorth <benignhumour@gmail.com>
2017-03-31 11:20:03 +0100

    Setup test file with first test to return the contents of the basket. Created an empty basket class file.

--- ShoppingBasketTest.java
+++ ShoppingBasketTest.java
@@ -1,8 +1,19 @@
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 public class ShoppingBasketTest {
 
-  
+  ShoppingBasket shoppingBasket;
+
+  @Before
+  public void before(){
+    shoppingBasket = new ShoppingBasket();
+  }
+
+  @Test
+  public void canGiveBasketContents(){
+    assertEquals(0, shoppingBasket.getBasketSize());
+  }
 
 }
