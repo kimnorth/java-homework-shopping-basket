d2b8924a0c0bf30148778099393975d5c5f0bd40
kimnorth <benignhumour@gmail.com>
2017-03-31 22:50:48 +0100

    In order to implement discounts at checkout, will need to re-write some code. Instead of a hashmap, will be an array of objects representing each shopping item. Creating a Product class for each shopping item.

---
+++ Product.java
@@ -0,0 +1,15 @@
+public class Product {
+
+  private String name;
+  private Integer price;
+  private Boolean bogof;
+
+  public Product(String name, Integer price, Boolean bogof){
+    this.name = name;
+    this.price = price;
+    this.bogof = bogof;
+  }
+
+  
+
+}
